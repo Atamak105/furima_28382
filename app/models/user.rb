532ee4df3406c1177c3b0cd6c3b9713655cda57f
Atamak105 <yk20195419@gmail.com>
2020-08-14 17:51:04 +0900

    userモデルのテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,15 @@ class User < ApplicationRecord
          
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   VALID_NAMEREADING_REGEX = /\A[ァ-ヶー－]+\z/
-  
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
 
   has_many :items
   has_many :comments
   has_many :item_purchases
 
   # 入力必須項目の設定 PW英数混合のバリデーション設定
-  validates :last_name, :first_name, :birthday, :nickname, :email, :encrypted_password, presence:true
+  validates :birthday, :nickname, :email, :encrypted_password, presence:true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name_reading, :first_name_reading, format: {with: VALID_NAMEREADING_REGEX}
+  validates :last_name, :first_name, format: {with: VALID_NAME_REGEX}
 end

8a443baeb272bc7b6de9cffe9a8eeebc36fd5fc5
Atamak105 <yk20195419@gmail.com>
2020-08-14 10:45:08 +0900

    ユーザーログイン機能（修正）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,18 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!,except: [:index, :show] #ログインしていない場合サインアップの画面に遷移する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # サインインしたらログインユーザーのTOP画面へ遷移する
-  def after_sign_in_path_for(resource)
-    user_session_path
-  end
+  # # サインインしたらログインユーザーのTOP画面へ遷移する（不要）
+  # def after_sign_in_path_for(resource)
+  #   user_session_path
+  # end
+
+  # サインアウトしたらTOPへ遷移（不要）
+  # def after_sign_out_path_for(resource)
+  #   root_path
+  # end
 
 
-  # サインアウトしたらTOPへ遷移
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-
-  private
+  protected
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
@@ -23,7 +22,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected
 
     #passwordとemailはデフォルトであるので不要
   def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAMEREADING_REGEX = /\A[ァ-ヶー－]+\z/
+  
 
   has_many :items
   has_many :comments
   has_many :item_purchases
 
-  # 入力必須項目の設定
-  validates :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday, :nickname, :email, :encrypted_password, presence:true
-
+  # 入力必須項目の設定 PW英数混合のバリデーション設定
+  validates :last_name, :first_name, :birthday, :nickname, :email, :encrypted_password, presence:true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :last_name_reading, :first_name_reading, format: {with: VALID_NAMEREADING_REGEX}
 end
